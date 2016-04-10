629ce2f44b61eeb13e4cb380201e4e26b41fee49
Francisco Maria <francisco@Franciscos-Air.lan>
2016-04-10 15:58:16 +0100

    retoques

--- transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterMain.java
+++ transporter-ws/src/main/java/pt/upa/transporter/ws/TransporterMain.java
@@ -18,12 +18,11 @@ public class TransporterMain {
 		String uddiURL = args[0];
 		String name = args[1];
 		String url = args[2];
-		int transporterNumber = Integer.parseInt(args[3]);
 
 		Endpoint endpoint = null;
 		UDDINaming uddiNaming = null;
 		try {
-			TransporterPort port = new TransporterPort(transporterNumber);
+			TransporterPort port = new TransporterPort(Integer.parseInt(name.substring(13, name.length() - 1)));
 			endpoint = Endpoint.create(port);
 
 			// publish endpoint
